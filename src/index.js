847ef46d2f1e004c0bc39f1caaef3a759225e4fb
cmamon <christophe.quenette@gmail.com>
2020-11-19 17:30:49 +0100

    Apply airbnb base code style

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const express = require("express");
-const helmet = require("helmet");
-const path = require("path");
-const logger = require("morgan");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const logger = require('morgan');
 
-const router = require("./router");
-const middlewares = require("./middlewares");
+const router = require('./router');
+const middlewares = require('./middlewares');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/", router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', router);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
-app.use(express.json)
+app.use(express.json);
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(cors());
 app.use(helmet());
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log("\x1b[32m%s\x1b[0m", "Server running on port " + port);
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://localhost:${port}`);
 });
--- src/middlewares.js
+++ src/middlewares.js
@@ -4,16 +4,17 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
   });
 };
 
 module.exports = {
   notFound,
-  errorHandler
+  errorHandler,
 };
